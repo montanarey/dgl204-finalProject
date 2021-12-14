13095cc11c3858bb564b9edc24b20fee9ecc5162
Montana Pistell <montana.pistell@hotmail.com>
2021-12-14 02:33:55 -0800

    Broken commit fix: add playground with settings and image/backgrounds structures
    File was saved in wrong location.

---
+++ KindWords_Settings.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+import Darwin
+
+/* --------------------------------
+    DGL 204: Prog Mobile App Dev
+    Final Project Fall 2021
+    Author: Montana Pistell n0145744
+    Last Modified: December 14, 2021
+    Kind Words: An app designed to encourage users through positive affirmations.
+ -------------------------------- */
+
+// Image struct: information about an image
+struct Image {
+    var url: String
+    var description: String
+}
+
+//Backgrounds class to hold images for background options
+class Backgrounds {
+    var images: [Image] = []
+    var selected: Image
+    
+    init() {
+        self.selected = images[0]
+    }
+    
+    func selectBackground(_ background: Image) {
+        selected = background
+    }
+    
+    func randomBackground() {
+        selected = images[Int.random(in: 1...images.count)]
+    }
+}
+

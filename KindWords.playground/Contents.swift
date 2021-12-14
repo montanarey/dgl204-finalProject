bf4b36f893e3e24ac12d669ddbf2f97abb8c270a
Montana Pistell <montana.pistell@hotmail.com>
2021-12-13 23:26:37 -0800

    Restructure Affirmation hierarchy to use protocol and class

--- KindWords.playground/Contents.swift
+++ KindWords.playground/Contents.swift
@@ -14,11 +14,47 @@ enum Category {
     case selfLove, gratitude, health, relationships, personality, motivation
 }
 
-// Affirmation protocol: information about any type of affirmation
-protocol Affirmation {
-    var content: String {get}
-    var category: Category {get}
-    var featured: Bool {get}
-    func toggle()
-    func display() -> String
+// Message protocol: the backbone of an affirmation. Has content, a display state, a toggle display method, and a category
+protocol Message {
+    var content: String {get set}
+    var display: Bool {get set} // whether the affirmation is currently being displayed
+    var category: Category? {get set}
+    func toggleDisplay()
+    init(content: String, category: Category?, display: Bool)
+    init(content: String, category: Category?)
+    init(content: String)
 }
+
+// Affirmation class: represents a single affirmation, which can be created with or without a category.
+class Affirmation: Message {
+    var content: String
+    var category: Category?
+    var display: Bool = false
+    
+    func toggleDisplay() {
+        display = !display
+    }
+    
+    required init(content: String, category: Category?, display: Bool) {
+        self.content = content
+        if let category = category {
+            self.category = category
+        }
+        self.display = display
+    }
+    
+    required init(content: String, category: Category?) {
+        self.content = content
+        if let category = category {
+            self.category = category
+        }
+    }
+    
+    required init(content: String) {
+        self.content = content
+    }
+}
+
+
+// ----- TEST CODE -----
+

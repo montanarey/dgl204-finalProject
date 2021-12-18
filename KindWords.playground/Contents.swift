8467ac3897cac09697c744fb06e50c9f6e736237
Montana Pistell <montana.pistell@hotmail.com>
2021-12-18 01:52:52 -0800

    Add grading rubric and update modified dates

--- KindWords.playground/Contents.swift
+++ KindWords.playground/Contents.swift
@@ -4,7 +4,7 @@ import Foundation
     DGL 204: Prog Mobile App Dev
     Final Project Fall 2021
     Author: Montana Pistell n0145744
-    Last Modified: December 14, 2021
+    Last Modified: December 18, 2021
     Kind Words: An app designed to encourage users through positive affirmations.
  -------------------------------- */
 
